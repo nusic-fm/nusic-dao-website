bb12c91ab23dc5f8cafbac888e7583bd5d4a0363
logesh2496 <you@example.com>
2022-06-06 21:59:19 +0530

    fix: with quality video and image

--- src/pages/Pfp/index.tsx
+++ src/pages/Pfp/index.tsx
@@ -10,90 +10,97 @@ const Pfp = () => {
         muted
         style={{ width: "100%", minHeight: "100vh", position: "fixed" }}
       >
-        <source src="/assets/pfp/v1.mp4" type="video/mp4" />
+        <source src="/assets/pfp/background.mp4" type="video/mp4" />
         Your browser does not support HTML5 video.
       </video>
       <Box position="absolute" width="100%" top="3%">
-        <FadeIn delay={14000} transitionDuration={1500}>
+        <FadeIn delay={9000} transitionDuration={1500}>
           <Typography variant="h2" align="center">
             INSPIRING SHIT
           </Typography>
         </FadeIn>
-        <FadeIn delay={15500} transitionDuration={1500}>
+        <FadeIn delay={10500} transitionDuration={1500}>
           <Typography variant="h2" align="center">
             MORE INSPIRING SHIT
           </Typography>
         </FadeIn>
       </Box>
-      <FadeIn delay={9000} transitionDuration={5000}>
-        <Box
-          position="absolute"
-          width="100%"
-          display="flex"
-          justifyContent="center"
-        >
-          <img src="/assets/pfp/avatar.png" alt="avatar" width="50%"></img>
+      <Box position="absolute" width="100%" bottom={0}>
+        <FadeIn delay={4000} transitionDuration={2500}>
           <Box
-            position="absolute"
-            top="90%"
             display="flex"
             justifyContent="center"
-            gap={20}
-            paddingLeft={10}
+            width="100%"
+            // bottom={0}
+            // zIndex={100}
           >
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                border: "1px solid rgba(255,255,255,0.4)",
-                fontWeight: "bold",
-              }}
+            <img
+              src="/assets/pfp/EDM-Isolated.svg"
+              alt="avatar"
+              width="40%"
+            ></img>
+            <Box
+              position="absolute"
+              top="90%"
+              display="flex"
+              justifyContent="center"
+              gap={20}
+              paddingLeft={10}
             >
-              Discord
-            </Button>
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                border: "1px solid rgba(255,255,255,0.4)",
-                fontWeight: "bold",
-              }}
-            >
-              Twitter
-            </Button>
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                border: "1px solid rgba(255,255,255,0.4)",
-                fontWeight: "bold",
-              }}
-            >
-              Github
-            </Button>
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                border: "1px solid rgba(255,255,255,0.4)",
-                fontWeight: "bold",
-              }}
-            >
-              Gitbook
-            </Button>
-            <Button
-              variant="contained"
-              style={{
-                backgroundColor: "rgba(0, 0, 0, 0.5)",
-                border: "1px solid rgba(255,255,255,0.4)",
-                fontWeight: "bold",
-              }}
-            >
-              Newsletter
-            </Button>
+              <Button
+                variant="contained"
+                style={{
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  border: "1px solid rgba(255,255,255,0.4)",
+                  fontWeight: "bold",
+                }}
+              >
+                Discord
+              </Button>
+              <Button
+                variant="contained"
+                style={{
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  border: "1px solid rgba(255,255,255,0.4)",
+                  fontWeight: "bold",
+                }}
+              >
+                Twitter
+              </Button>
+              <Button
+                variant="contained"
+                style={{
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  border: "1px solid rgba(255,255,255,0.4)",
+                  fontWeight: "bold",
+                }}
+              >
+                Github
+              </Button>
+              <Button
+                variant="contained"
+                style={{
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  border: "1px solid rgba(255,255,255,0.4)",
+                  fontWeight: "bold",
+                }}
+              >
+                Gitbook
+              </Button>
+              <Button
+                variant="contained"
+                style={{
+                  backgroundColor: "rgba(0, 0, 0, 0.5)",
+                  border: "1px solid rgba(255,255,255,0.4)",
+                  fontWeight: "bold",
+                }}
+              >
+                Newsletter
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </FadeIn>
+        </FadeIn>
+      </Box>
     </Box>
   );
 };
